ec061ed7f02aaa8753f45e176678c938ac25283c
Valentin1495 <newface95@icloud.com>
2023-08-08 00:54:34 +0900

    Build CityPicker component

--- components/city-picker.tsx
+++ components/city-picker.tsx
@@ -1,8 +1,68 @@
-import { City } from 'country-state-city';
+'use client';
+
+import { City, Country } from 'country-state-city';
+import { useState } from 'react';
+import Select from 'react-select';
+
+type CountryOption = {
+  value: string;
+  label: string;
+  code: string;
+} | null;
+
+type CityOption = {
+  value: string;
+  label: string;
+} | null;
 
 type Props = {};
+
 export default function CityPicker({}: Props) {
-  const allCities = City.getAllCities();
-  console.log(allCities);
-  return <div>city-picker</div>;
+  const allCountries = Country.getAllCountries();
+  const countryOptions = allCountries.map((country) => {
+    return {
+      value: country.name,
+      label: country.name,
+      code: country.isoCode,
+    };
+  });
+
+  const [selectedCountry, setSelectedCountry] = useState<CountryOption>(null);
+  const [selectedCity, setSelectedCity] = useState<CityOption>(null);
+
+  const handleCountryChange = (countryOption: CountryOption) => {
+    setSelectedCountry(countryOption);
+  };
+  const handleCityChange = (cityOption: CityOption) => {
+    setSelectedCity(cityOption);
+  };
+
+  const countryCode = selectedCountry?.code;
+
+  const cityOptions = City.getCitiesOfCountry(countryCode as string)?.map(
+    (city) => {
+      return {
+        value: city.name,
+        label: city.name,
+      };
+    }
+  );
+
+  return (
+    <div>
+      <Select
+        options={countryOptions}
+        value={selectedCountry}
+        onChange={handleCountryChange}
+      />
+
+      {selectedCountry && (
+        <Select
+          options={cityOptions}
+          value={selectedCity}
+          onChange={handleCityChange}
+        />
+      )}
+    </div>
+  );
 }
