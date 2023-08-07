0f5d897429b2f9481264833460124417bd07b0c6
Valentin1495 <newface95@icloud.com>
2023-08-07 23:58:57 +0900

    Install react-select

--- components/city-picker.tsx
+++ components/city-picker.tsx
@@ -1,4 +1,8 @@
+import { City } from 'country-state-city';
+
 type Props = {};
 export default function CityPicker({}: Props) {
+  const allCities = City.getAllCities();
+  console.log(allCities);
   return <div>city-picker</div>;
 }
