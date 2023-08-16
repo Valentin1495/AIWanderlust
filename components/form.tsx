5cc0e172dd247d60af1182524fcf7b6a9aaf5b1d
Valentin1495 <newface95@icloud.com>
2023-08-16 23:40:39 +0900

    Build header

--- components/form.tsx
+++ components/form.tsx
@@ -5,6 +5,7 @@ import { useMultistepForm } from '@/hooks/useMultistepForm';
 import { useRouter } from 'next/navigation';
 import { FormEvent, useState } from 'react';
 import TripLength from './trip-length';
+import { BotIcon } from './icons';
 
 type FormData = {
   place: string;
@@ -20,6 +21,7 @@ const INITIAL_DATA = {
 
 export default function Form() {
   const [data, setData] = useState<FormData>(INITIAL_DATA);
+  const [areCleared, setAreCleared] = useState<boolean>(false);
   const router = useRouter();
   const { place, tripLength, numOfPeople } = data;
 
@@ -39,24 +41,37 @@ export default function Form() {
     next,
     back,
   } = useMultistepForm([
-    <PlaceCombobox {...data} updateFields={updateFields} />,
+    <PlaceCombobox
+      {...data}
+      updateFields={updateFields}
+      setAreCleared={setAreCleared}
+    />,
     <TripLength />,
   ]);
+  const indexOfComma = place.includes(',') ? place.indexOf(',') : place.length;
+  const formattedPlace = place.slice(0, indexOfComma);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     if (!place.trim()) return;
     if (!isLastStep) return next();
 
-    const formattedPlace = place.replace(',', '').split(' ').join('+');
-
     router.push(
       `/${formattedPlace}+itinerary?tripLength=${tripLength}&numOfPeople=${numOfPeople}`
     );
   };
+  const progress = (currentStepIndex + 1 / steps.length) * 100;
 
   return (
     <div>
+      <div className='fixed top-0 inset-x-0 flex flex-col items-center justify-center h-20 shadow-md'>
+        <span className='text-sm flex items-center gap-x-2'>
+          <BotIcon className='w-6 h-6' /> Powered by AI
+        </span>
+        {formattedPlace && areCleared && (
+          <h4 className='font-bold'>{formattedPlace} itinerary</h4>
+        )}
+      </div>
       <form onSubmit={handleSubmit}>
         {step}
         <div>
@@ -65,13 +80,7 @@ export default function Form() {
               Back
             </button>
           )}
-          <div
-            className={`${
-              !place.trim() ? 'justify-between' : 'justify-end'
-            } fixed bottom-0 inset-x-0 shadow-top flex items-center px-5 h-20`}
-          >
-            {!place.trim() && <p className=''>Please select a location</p>}
-
+          <div className='justify-end fixed bottom-0 inset-x-0 shadow-top flex items-center px-5 h-20'>
             <button
               type='submit'
               className='bg-black text-white px-[72px] py-3 rounded-full font-bold hover:opacity-80 transition-opacity h-fit'
--- components/place-combobox.tsx
+++ components/place-combobox.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Dispatch, SetStateAction } from 'react';
 import usePlacesAutocomplete from 'use-places-autocomplete';
 
 type Place = {
@@ -9,9 +10,14 @@ type Place = {
 type Props = {
   place: string;
   updateFields: (fields: Partial<Place>) => void;
+  setAreCleared: Dispatch<SetStateAction<boolean>>;
 };
 
-export default function PlaceCombobox({ place, updateFields }: Props) {
+export default function PlaceCombobox({
+  place,
+  updateFields,
+  setAreCleared,
+}: Props) {
   const {
     ready,
     setValue,
@@ -22,7 +28,7 @@ export default function PlaceCombobox({ place, updateFields }: Props) {
   const handleSelect = async (val: string) => {
     setValue(val, false);
     clearSuggestions();
-
+    setAreCleared(true);
     // const results = await getGeocode({ address: val });
     // const { lat, lng } = getLatLng(results[0]);
     // setSelectedPlace({ address: val, lat, lng });
@@ -30,21 +36,23 @@ export default function PlaceCombobox({ place, updateFields }: Props) {
   };
 
   return (
-    <div className='space-y-3'>
+    <div className='space-y-3 pt-10'>
       <h1 className='text-3xl font-bold text-neutral-600 mb-5'>
         Where do you want to go?
       </h1>
       <h4 className='text-sm text-neutral-600'>Select a City/Town</h4>
       <section className='space-y-2'>
         <input
+          required
           value={place}
           onChange={(e) => {
+            setAreCleared(false);
             updateFields({ place: e.target.value });
             setValue(e.target.value);
           }}
           disabled={!ready}
           placeholder='Where to?'
-          className='rounded-full shadow-custom px-5 py-3 outline-none text-neutral-500'
+          className='rounded-full shadow-custom px-5 py-3 outline-none text-neutral-500 w-full'
         />
         {status === 'OK' && (
           <ul className='shadow-md rounded-md'>
