d9c6aad65c2542bda6537c2d38f5e1b8a2f34f90
Valentin1495 <newface95@icloud.com>
2023-08-26 14:36:40 +0900

    Build home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+import { BotIcon, PlaneIcon } from '@/components/icons';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className='bg-orange-50 rounded-md flex flex-col items-center gap-y-3 p-5'>
+      <h6 className='text-orange-600 flex items-center gap-x-2'>
+        <span>Powered by AI</span>
+        <BotIcon className='w-5 h-5' />{' '}
+      </h6>
+      <h1 className='font-bold text-4xl text-orange-600'>
+        Build a trip in minutes
+      </h1>
+      <p className='font-bold text-xl text-orange-600'>
+        Get a personalized itinerary just for you, guided by traveler tips and
+        reviews.
+      </p>
+      <Link
+        href={'/generate-itinerary'}
+        className='bg-orange-600 text-white font-bold rounded-full gap-x-2 text-center flex px-5 py-3 hover:bg-opacity-90 transition-all mt-3'
+      >
+        <PlaneIcon className='w-6 h-6' /> Start a trip with AI
+      </Link>
+    </div>
+  );
 }
--- components/icons.tsx
+++ components/icons.tsx
@@ -147,3 +147,20 @@ export const HeartIcon = ({ className }: { className: string }) => (
     <path d='M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z' />
   </svg>
 );
+
+export const PlaneIcon = ({ className }: { className: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className={className}
+  >
+    <path d='M17.8 19.2 16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z' />
+  </svg>
+);
