22432576a1b4e2847dbfdc2a4d9c71d1dd323f6b
Valentin1495 <newface95@icloud.com>
2023-08-30 21:25:50 +0900

    Enable keyboard navigation through combobox

--- components/place-combobox.tsx
+++ components/place-combobox.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, KeyboardEvent, SetStateAction, useState } from 'react';
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
@@ -33,13 +33,36 @@ export default function PlaceCombobox({
     updateFields({ place: val, lat, lng });
   };
 
+  const [focusedItemIndex, setFocusedItemIndex] = useState<number>(-1);
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case 'ArrowUp':
+        setFocusedItemIndex((prevIndex) => Math.max(0, prevIndex - 1));
+        break;
+      case 'ArrowDown':
+        setFocusedItemIndex((prevIndex) =>
+          Math.min(data.length - 1, prevIndex + 1)
+        );
+        break;
+      case 'Enter':
+        if (focusedItemIndex >= 0) {
+          handleSelect(data[focusedItemIndex].description);
+        }
+        break;
+    }
+  };
+
   return (
     <div className='space-y-5 pt-10 flex flex-col items-center'>
       <h1 className='text-3xl font-bold text-neutral-600 mb-5'>
         Where do you want to go?
       </h1>
       <div className='space-y-2'>
-        <section className='rounded-full flex items-center gap-x-3 shadow-custom text-neutral-500 px-5 py-3 w-96'>
+        <section
+          tabIndex={0}
+          className='rounded-full flex items-center gap-x-3 shadow-custom text-neutral-500 px-5 py-3 w-96'
+        >
           <SearchIcon className='w-6 h-6' />
           <input
             required
@@ -49,6 +72,7 @@ export default function PlaceCombobox({
               updateFields({ place: e.target.value });
               setValue(e.target.value);
             }}
+            onKeyDown={handleKeyDown}
             disabled={!ready}
             placeholder='Search by city or town'
             className='outline-none w-full'
@@ -56,10 +80,14 @@ export default function PlaceCombobox({
         </section>
         {status === 'OK' && (
           <ul className='shadow-md rounded-md w-96'>
-            {data.map(({ place_id, description }) => (
+            {data.map(({ place_id, description }, i) => (
               <li
                 key={place_id}
-                className='hover:bg-neutral-100 p-3 cursor-pointer'
+                className={`${
+                  i === focusedItemIndex
+                    ? 'bg-neutral-100'
+                    : 'hover:bg-neutral-100'
+                } p-3 cursor-pointer`}
                 onClick={() => handleSelect(description)}
               >
                 {description}
