2b212ee5d9b20f2dd12a2c616bed09e0213db035
Valentin1495 <newface95@icloud.com>
2023-09-06 22:25:47 +0900

    Add example to the prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -25,17 +25,39 @@ export async function POST(req: Request) {
       // optional, preamble context to prime responses
       // context: ''
       // Optional. Examples for further fine-tuning of responses.
-      //   examples: [
-      //     {
-      //       input: {
-      //         content: `
-      //       },
-      //       output: {
-      //         content: `
-      //         `,
-      //       },
-      //     },
-      //   ],
+      examples: [
+        {
+          input: {
+            content: `
+              Can you curate a tour to paris for a day? Make sure it is really short and simple.
+              `,
+          },
+          output: {
+            content: `
+**Morning:**
+1. **Eiffel Tower**: Start your day early at the iconic Eiffel Tower. You can admire its beauty from the outside or choose to go up to the second floor for panoramic views of the city (consider booking tickets in advance to skip the lines).
+
+**Lunch:**
+2. **Café Break**: Head to a charming Parisian café nearby for a leisurely French breakfast or brunch. Try croissants, café au lait, or a classic omelette.
+
+**Afternoon:**
+3. **Louvre Museum**: Visit the world-famous Louvre Museum, home to the Mona Lisa and many other remarkable artworks. You can explore the highlights in a couple of hours.
+
+**Late Afternoon:**
+4. **Seine River Cruise**: Take a relaxing Seine River cruise to see Paris from a different perspective. These tours often depart near the Eiffel Tower and provide great photo opportunities.
+
+**Evening:**
+5. **Notre-Dame Cathedral**: Stroll over to the Notre-Dame Cathedral and admire its stunning architecture from the outside.
+
+**Dinner:**
+6. **Dinner in Le Marais**: Enjoy a delicious dinner in the charming Le Marais district. You'll find many restaurants offering a variety of French and international cuisines.
+
+**Night:**
+7. **Illuminated Eiffel Tower**: End your day by returning to the Eiffel Tower in the evening. It's beautifully illuminated after sunset, creating a magical atmosphere.
+              `,
+          },
+        },
+      ],
       // Required. Alternating prompt/response messages.
       messages,
     },
